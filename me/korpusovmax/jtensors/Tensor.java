55eacf02bc05ed062911da40efbe6728b2d41b42
Korpusov Maxim <65653941+korpusovmax@users.noreply.github.com>
2021-12-21 23:10:22 +0300

    Create Tensor.java

---
+++ me/korpusovmax/jtensors/Tensor.java
@@ -0,0 +1,31 @@
+package me.korpusovmax.sann;
+
+public interface Tensor {
+
+    int size();
+    int dims();
+    int[] shape();
+    String toString();
+    Tensor copy();
+
+    default Tensor transpose() {
+        return this;
+    }
+    default Tensor transpose(int axis) {
+        return this.transpose();
+    }
+
+    Tensor operate(Tensor l, Tensors.Operation op);
+    default Tensor add(Tensor l){
+        return this.operate(l, Tensors.Add);
+    }
+    default Tensor sub(Tensor l){
+        return this.operate(l, Tensors.Sub);
+    }
+    default Tensor div(Tensor l){
+        return this.operate(l, Tensors.Div);
+    }
+    default Tensor mul(Tensor l){
+        return this.operate(l, Tensors.Mul);
+    }
+}
